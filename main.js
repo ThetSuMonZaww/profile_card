85ca12af1da3c4ccc0847f4df32ddfb3cd33af4f
ThetSuMonZaww <thetsumonzaw92@gmail.com>
2022-05-19 13:30:46 +0630

    commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.scss'
+
+import 'bootstrap';
+import 'waypoints/lib/noframework.waypoints.min';
+import ScrollReveal from "scrollreveal";
+import Typed from "typed.js";
+
+
+ScrollReveal().reveal('.profile-box', { delay: 1000 });
